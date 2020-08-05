bbfc92a74bbe501e8fa6a830d8eb590871a18427
SrikanthVH <68265642+SrikanthVH@users.noreply.github.com>
2020-08-05 09:54:20 +0530

    Testing
    Testing

--- web/dss-dashboard/src/actions/getPDFHeaderDetails.js
+++ web/dss-dashboard/src/actions/getPDFHeaderDetails.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 export default function getPDFHeaderDetails(mdmsData) {
-  let returnObj = {logo: '',headerText: ''};
+  console.log("Check mdms Data", mdmsData);
+  let returnObj = {logo: '',headerText: 'TEST'};
   if(!_.isEmpty(mdmsData,true)){
     returnObj.logo = mdmsData['tentantLogo'][`${localStorage.getItem('tenant-id')}`];
     returnObj.headerText= mdmsData['tenantName'].toUpperCase() +" "+ mdmsData['corpName'].toUpperCase();    
