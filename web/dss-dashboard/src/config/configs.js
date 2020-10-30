520156d931bfd3ebc67169755a67516b3b9b817c
SrikanthVH <68265642+SrikanthVH@users.noreply.github.com>
2020-10-30 11:49:18 +0530

    Config updated for Water and Sewerage
    Config updated for Water and Sewerage

--- web/dss-dashboard/src/config/configs.js
+++ web/dss-dashboard/src/config/configs.js
@@ -29,7 +29,7 @@ const configs = {
                     {'pgr':{'services_name':'PGR','filterKey':'PGR'}},
                     {'ws':{'services_name':'W&S','filterKey':'W&S'}}
                     ],
-    'SERVICES': ["Trade licence", "M Collect", "PGR"]
+    'SERVICES': ["Trade licence", "M Collect", "PGR", "Water & Sewerage"]
     //'SERVICES': ["Property Tax", "Trade licence","PGR", "M Collect"]
 }
 export default configs;
