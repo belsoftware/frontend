0e8767f92577654e80dd990f91477a67b59f2d78
Rajanihn <rajnihn@gmail.com>
2021-02-27 11:49:13 +0530

    Revert "Migrate to 2.2"
    This reverts commit 8d613b5b8f083522000d6f966ae79dd397ea5edf.

--- web/rainmaker/dev-packages/pt-citizen-dev/src/Screens/FormWizard/index.js
+++ web/rainmaker/dev-packages/pt-citizen-dev/src/Screens/FormWizard/index.js
@@ -35,7 +35,6 @@ import PlotDetails from "./components/Forms/PlotDetails";
 import WizardComponent from "./components/WizardComponent";
 import "./index.css";
 import { getDocumentTypes } from "./utils/mdmsCalls";
-import { generalMDMSDataRequestObj, getGeneralMDMSDataDropdownName } from "egov-ui-kit/utils/commons";
 
 class FormWizard extends Component {
   state = {
@@ -288,8 +287,6 @@ class FormWizard extends Component {
       const propertyId = getQueryValue(search, "propertyId");
       const draftUuid = getQueryValue(search, "uuid");
       fetchLocalizationLabel(getLocale(), tenantId, tenantId);
-      let requestBody = generalMDMSDataRequestObj(commonConfig.tenantId);
-      fetchGeneralMDMSData(requestBody, "PropertyTax", getGeneralMDMSDataDropdownName()); 
       const documentTypeMdms = await getDocumentTypes();
       if (!!documentTypeMdms) fetchMDMDDocumentTypeSuccess(documentTypeMdms);
       this.unlisten = history.listen((location, action) => {
