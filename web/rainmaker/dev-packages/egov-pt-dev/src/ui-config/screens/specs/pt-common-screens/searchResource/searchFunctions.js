624f7ed01f0384b756a4fcb268ef5eabf6f76a22
Vidyabhushan300 <itsvidyabhushan@gmail.com>
2021-05-20 14:10:12 +0530

    search property based on old property code and abas id

--- web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-common-screens/searchResource/searchFunctions.js
+++ web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-common-screens/searchResource/searchFunctions.js
@@ -53,7 +53,8 @@ const searchApiCall = async (state, dispatch) => {
   }  else if (
     (searchScreenObject["propertyIds"] === undefined || searchScreenObject["propertyIds"] === "") &&
     (searchScreenObject["mobileNumber"] === undefined || searchScreenObject["mobileNumber"] === "") &&
-    (searchScreenObject["oldpropertyids"] === undefined || searchScreenObject["oldpropertyids"] === "")
+    (searchScreenObject["oldpropertyids"] === undefined || searchScreenObject["oldpropertyids"] === "") &&
+    (searchScreenObject["abasPropertyids"] === undefined || searchScreenObject["abasPropertyids"] === "")
   ) {
     dispatch(toggleSnackbar(true, { labelName:"In addition to CB, please provide any one of the other parameters to search for property.",labelKey: "ERR_PT_COMMON_FILL_VALID_FIELDS" }, "warning"));
   }else{
