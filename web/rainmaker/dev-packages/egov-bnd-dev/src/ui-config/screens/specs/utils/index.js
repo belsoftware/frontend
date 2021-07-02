39597471c4617d0fa5e56baa14b151675a718bc5
SrikanthVH <shrknth@gmail.com>
2021-07-02 17:11:58 +0530

    Testing download - to be reverted
    Testing download - to be reverted

--- web/rainmaker/dev-packages/egov-bnd-dev/src/ui-config/screens/specs/utils/index.js
+++ web/rainmaker/dev-packages/egov-bnd-dev/src/ui-config/screens/specs/utils/index.js
@@ -15,9 +15,39 @@ import store from "ui-redux/store";
 import { openPdf, printPdf } from "egov-ui-kit/utils/commons";
 import { getFileUrlFromAPI } from "egov-ui-framework/ui-utils/commons";
 
+export const DownloadFile = (url) => {
+  //Set the File URL.
+  alert("Trigger now");
+
+  //Create XMLHTTP Request.
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.responseType = "blob";
+  req.onload = function () {
+      //Convert the Byte Data to BLOB object.
+      var blob = new Blob([req.response], { type: "application/octetstream" });
+
+      //Check the Browser type and download the File.
+      var isIE = false || !!document.documentMode;
+      if (isIE) {
+          window.navigator.msSaveBlob(blob, fileName);
+      } else {
+          var url = window.URL || window.webkitURL;
+          link = url.createObjectURL(blob);
+          var a = document.createElement("a");
+          a.setAttribute("download", fileName);
+          a.setAttribute("href", link);
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+      }
+  };
+  req.send();
+};
 
 export const downloadPdf = (link, openIn = '_blank') => {
-  var win = window.open(link, '_self');
+  //var win = window.open(link, '_self');
+  DownloadFile(link);
   if (win) {
     win.focus();
   }
