c3b7f4d5bef97b101a5776dca42d9dde6936aff2
SrikanthVH <shrknth@gmail.com>
2021-07-17 22:00:04 +0530

    Updates to prev commit
    Updates to prev commit

--- web/rainmaker/dev-packages/egov-obm-dev/src/ui-containers-local/Table/workflowRedirectionConfig.js
+++ web/rainmaker/dev-packages/egov-obm-dev/src/ui-containers-local/Table/workflowRedirectionConfig.js
@@ -2,8 +2,8 @@ export const getWFConfig = (module, businessService) => {
   switch (module.toUpperCase()) {
     case "OBM-SERVICES":
       return {
-          CITIZEN: "/obm-common/viewBooking", //tobechanged  If Citizen has to resubmit the documents.
-          DEFAULT: "/obm-common/viewBooking",
+          CITIZEN: "/chb-common/viewBooking", //tobechanged  If Citizen has to resubmit the documents.
+          DEFAULT: "/chb-common/viewBooking",
       };
   }
 };
